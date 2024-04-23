a717f93b7063d7c9c61f5cd7aa3ef82d81097632
Vellanos <davidbak38@gmail.com>
2024-04-23 09:39:19 +0200

    ezAdmin config

---
+++ src/Controller/Admin/FighterCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Fighter;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class FighterCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Fighter::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
