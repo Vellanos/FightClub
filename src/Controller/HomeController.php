abf8a2c9e917bc6c049df2fb663e7a69b5ce102f
Vellanos <davidbak38@gmail.com>
2024-04-29 09:01:22 +0200

    save lundi 29

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,8 +16,38 @@ class HomeController extends AbstractController
         $duelRepository = $entityManager->getRepository(Duel::class);
         $duels = $duelRepository->findAll();
 
+        $cotes = [];
+
+        foreach ($duels as $index => $duel) {
+
+            $fighter1 = $duel->getFighter1();
+            $level_1 = $fighter1->getLevel();
+
+            $fighter2 = $duel->getFighter2();
+            $level_2 = $fighter2->getLevel();
+
+            $cotes[$index] = calculerCote($level_1, $level_2);
+        }
+
         return $this->render('home/index.html.twig', [
             'duels' => $duels,
+            'cotes' => $cotes
         ]);
     }
 }
+
+function calculerCote($noteCombattant1, $noteCombattant2) {
+    $probabilite1 = $noteCombattant1 / ($noteCombattant1 + $noteCombattant2);
+    $probabilite2 = $noteCombattant2 / ($noteCombattant1 + $noteCombattant2);
+
+    if ($noteCombattant1 == $noteCombattant2) {
+        $probabilite1 = 0.5;
+        $probabilite2 = 0.5;
+    }
+
+    $cote1 = round(1 / $probabilite1, 2);
+    $cote2 = round(1 / $probabilite2, 2);
+
+    return [$cote1, $cote2];
+}
+
